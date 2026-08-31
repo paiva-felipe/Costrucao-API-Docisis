a35397c14cbab2b3000c5eb5a8114480f77f4bb1
GustavoJustin <gustavo.j.francisco@aluno.senai.br>
2026-08-31 11:27:34 -0300

    criação_relatorio_analises

---
+++ front-end/tailwind/script.js
@@ -0,0 +1,4 @@
+<button type="button" onclick="const i=document.getElementById('senha'); i.type = i.type==='password' ? 'text' : 'password';"
+              class="absolute right-3 top-1/2 -translate-y-1/2 text-cocoa-400 hover:text-cocoa-600 text-xs">
+              mostrar
+            </button>
