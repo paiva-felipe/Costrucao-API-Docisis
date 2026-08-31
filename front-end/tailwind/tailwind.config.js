86c2dcbb645f8a64845d6c48780c4bf7bbc9dc6a
paiva-felipe <felipe.g.paiva7@aluno.senai.br>
2026-08-31 09:09:52 -0300

    alterações

--- front-end/tailwind/tailwind.config.js
+++ front-end/tailwind/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.html",
+    "./**/*.html",
+    "./*.js",
+    "./**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
